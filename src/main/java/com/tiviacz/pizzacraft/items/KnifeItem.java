cbb74d0b85308f414d3b553c99f3f247ec065233
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-08-07 13:46:01 +0200

    🐛 Fixed crash on server while attacking with knife

--- src/main/java/com/tiviacz/pizzacraft/items/KnifeItem.java
+++ src/main/java/com/tiviacz/pizzacraft/items/KnifeItem.java
@@ -9,7 +9,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -80,6 +82,8 @@ public class KnifeItem extends SwordItem
 
             if(victim != null && attacker != null)
             {
+                Level level = attacker.getLevel();
+
                 if(attacker instanceof LivingEntity)
                 {
                     if(compareRotations(attacker.yRotO, victim.yRotO, 50.0D))
@@ -94,11 +98,18 @@ public class KnifeItem extends SwordItem
                             {
                                 float newDamage = event.getAmount() * 1.25F;
                                 event.setAmount(newDamage);
-                                attacker.getLevel().playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, attacker.getSoundSource(), 1.0F, 1.2F);
+                                level.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, attacker.getSoundSource(), 1.0F, 1.2F);
 
-                                if(attacker.getLevel().isClientSide())
+                                if(level instanceof ServerLevel serverLevel)
                                 {
-                                    Minecraft.getInstance().particleEngine.createTrackingEmitter(victim, ParticleTypes.CRIT, 10);
+                                    float f = level.random.nextFloat() * (float) Math.PI * 2.0F;
+                                    float f1 = level.random.nextFloat() * 0.5F + 0.5F;
+                                    float f2 = Mth.sin(f) * 0.5F * f1;
+                                    float f3 = Mth.cos(f) * 0.5F * f1;
+                                    serverLevel.sendParticles(ParticleTypes.CRIT,
+                                            victim.position().x + f2,
+                                            victim.getBoundingBox().minY + level.random.nextFloat() + 0.5F,
+                                            victim.position().z + f3, 8, (double)(float)Math.pow(2.0D, (level.random.nextInt(169) - 12) / 12.0D) / 24.0D, -1.0D, 0.0D, 0.1);
                                 }
                             }
                         }
